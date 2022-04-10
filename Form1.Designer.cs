1645236ddd4bfbba939878054855788f52d35c2e
dolay-demirel <102023873+dolay-demirel@users.noreply.github.com>
2022-04-11 01:25:25 +0300

    Lab1 changes were made (previous commit was wrong)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,31 +36,36 @@ namespace Prelab_1
             this.lbl_password = new System.Windows.Forms.Label();
             this.btn_login = new System.Windows.Forms.Button();
             this.btn_cancel = new System.Windows.Forms.Button();
+            this.checkbox_showpass = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // lbl_title
             // 
             this.lbl_title.AutoSize = true;
             this.lbl_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_title.Location = new System.Drawing.Point(150, 25);
+            this.lbl_title.Location = new System.Drawing.Point(200, 31);
+            this.lbl_title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_title.Name = "lbl_title";
-            this.lbl_title.Size = new System.Drawing.Size(69, 24);
+            this.lbl_title.Size = new System.Drawing.Size(87, 29);
             this.lbl_title.TabIndex = 0;
             this.lbl_title.Text = "Log-in";
             // 
             // txtbox_username
             // 
-            this.txtbox_username.Location = new System.Drawing.Point(125, 60);
+            this.txtbox_username.Location = new System.Drawing.Point(167, 74);
+            this.txtbox_username.Margin = new System.Windows.Forms.Padding(4);
             this.txtbox_username.Name = "txtbox_username";
-            this.txtbox_username.Size = new System.Drawing.Size(175, 20);
+            this.txtbox_username.Size = new System.Drawing.Size(232, 22);
             this.txtbox_username.TabIndex = 1;
             this.txtbox_username.TextChanged += new System.EventHandler(this.txtbox_username_TextChanged);
+            this.txtbox_username.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbox_username_KeyPress);
             // 
             // txtbox_password
             // 
-            this.txtbox_password.Location = new System.Drawing.Point(125, 86);
+            this.txtbox_password.Location = new System.Drawing.Point(167, 106);
+            this.txtbox_password.Margin = new System.Windows.Forms.Padding(4);
             this.txtbox_password.Name = "txtbox_password";
-            this.txtbox_password.Size = new System.Drawing.Size(175, 20);
+            this.txtbox_password.Size = new System.Drawing.Size(232, 22);
             this.txtbox_password.TabIndex = 2;
             this.txtbox_password.TextChanged += new System.EventHandler(this.txtbox_password_TextChanged);
             // 
@@ -68,9 +73,10 @@ namespace Prelab_1
             // 
             this.lbl_username.AutoSize = true;
             this.lbl_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_username.Location = new System.Drawing.Point(45, 61);
+            this.lbl_username.Location = new System.Drawing.Point(60, 75);
+            this.lbl_username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_username.Name = "lbl_username";
-            this.lbl_username.Size = new System.Drawing.Size(74, 16);
+            this.lbl_username.Size = new System.Drawing.Size(91, 20);
             this.lbl_username.TabIndex = 3;
             this.lbl_username.Text = "Username:";
             // 
@@ -78,18 +84,20 @@ namespace Prelab_1
             // 
             this.lbl_password.AutoSize = true;
             this.lbl_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lbl_password.Location = new System.Drawing.Point(45, 87);
+            this.lbl_password.Location = new System.Drawing.Point(60, 107);
+            this.lbl_password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(71, 16);
+            this.lbl_password.Size = new System.Drawing.Size(88, 20);
             this.lbl_password.TabIndex = 4;
             this.lbl_password.Text = "Password:";
             this.lbl_password.Click += new System.EventHandler(this.lbl_password_Click);
             // 
             // btn_login
             // 
-            this.btn_login.Location = new System.Drawing.Point(48, 120);
+            this.btn_login.Location = new System.Drawing.Point(64, 148);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(4);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(75, 25);
+            this.btn_login.Size = new System.Drawing.Size(100, 31);
             this.btn_login.TabIndex = 5;
             this.btn_login.Text = "Login";
             this.btn_login.UseVisualStyleBackColor = true;
@@ -97,19 +105,32 @@ namespace Prelab_1
             // 
             // btn_cancel
             // 
-            this.btn_cancel.Location = new System.Drawing.Point(225, 120);
+            this.btn_cancel.Location = new System.Drawing.Point(300, 148);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4);
             this.btn_cancel.Name = "btn_cancel";
-            this.btn_cancel.Size = new System.Drawing.Size(75, 25);
+            this.btn_cancel.Size = new System.Drawing.Size(100, 31);
             this.btn_cancel.TabIndex = 6;
             this.btn_cancel.Text = "Cancel";
             this.btn_cancel.UseVisualStyleBackColor = true;
             this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
             // 
+            // checkbox_showpass
+            // 
+            this.checkbox_showpass.AutoSize = true;
+            this.checkbox_showpass.Location = new System.Drawing.Point(333, 13);
+            this.checkbox_showpass.Name = "checkbox_showpass";
+            this.checkbox_showpass.Size = new System.Drawing.Size(125, 20);
+            this.checkbox_showpass.TabIndex = 7;
+            this.checkbox_showpass.Text = "Show Password";
+            this.checkbox_showpass.UseVisualStyleBackColor = true;
+            this.checkbox_showpass.CheckedChanged += new System.EventHandler(this.checkbox_showpass_CheckedChanged);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(349, 161);
+            this.ClientSize = new System.Drawing.Size(465, 198);
+            this.Controls.Add(this.checkbox_showpass);
             this.Controls.Add(this.btn_cancel);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.lbl_password);
@@ -118,8 +139,10 @@ namespace Prelab_1
             this.Controls.Add(this.txtbox_username);
             this.Controls.Add(this.lbl_title);
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Log-in";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
@@ -136,6 +159,7 @@ namespace Prelab_1
         private System.Windows.Forms.Label lbl_password;
         private System.Windows.Forms.Button btn_login;
         private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.CheckBox checkbox_showpass;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace Prelab_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            txtbox_username.Text = Properties.Settings.Default.remember_username;
         }
 
         private void txtbox_username_TextChanged(object sender, EventArgs e)
@@ -70,6 +70,34 @@ namespace Prelab_1
             if (e.KeyCode == Keys.Enter)
             {
                 btn_login.PerformClick();
+            }
+        }
+
+        private void txtbox_username_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsLetter(e.KeyChar)) && (!char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.remember_username = txtbox_username.Text;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkbox_showpass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbox_showpass.Checked)
+            {
+                txtbox_password.PasswordChar = '\0';
+            }
+
+            else
+            {
+                txtbox_password.PasswordChar = '*';
             }
         }
     }
